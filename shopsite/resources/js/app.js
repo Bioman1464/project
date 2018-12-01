69816561778603d17c9c514c090ca376fdd6f12a
Nikita <Karelison97@gmail.com>
2018-12-01 18:36:15 +0300

    .

--- shopsite/resources/js/app.js
+++ shopsite/resources/js/app.js
@@ -9,6 +9,7 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.use(require('vue-moment'));
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -18,6 +19,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('articles', require('./components/Articles.vue'));
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
---
+++ shopsite/resources/js/components/Articles.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class='articles'>
+
+        <!-- <article v-for="article in articles" v-bind:key="article.id">
+            <h3>{{ article.title }}</h3>
+            <span>{{ article.created_at | moment("DD.MM.YYYY") }}</span>
+            <h4>Categories:</h4>
+            <ul>
+              <li v-for="category in article.categories" v-bind:key="category.id">
+                <a href="#">{{category.title}}</a>
+              </li>
+            </ul>
+            <p>
+                {{ article.content }}
+            </p> -->
+            <div>
+                fdsfsdfsf
+            </div>
+            <!-- <button @click="deleteArticle(article.id)" class="btn btn-danger">Delete</button> -->
+        <!-- </article> -->
+
+    </main>
+</template>
+
+<style></style>
+
+<script>
+    import { API } from "../api-common.js";
+</script>
