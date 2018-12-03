b492724502022d74f4fa101a8905d90a92da821f
Bioman1464 <danilgap1464@gmail.com>
2018-12-03 18:36:42 +0400

    dad1

--- shopsite/database/seeds/DatabaseSeeder.php
+++ shopsite/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             // UsersTableSeeder::class
             CategoriesSeeder::class,
-            ArticlesSeeder::class
+            ItemsSeeder::class
         ]);
     }
 }
