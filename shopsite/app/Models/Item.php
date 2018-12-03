da817aa140203245f61ef5f7e44ac3895bc54db3
Bioman1464 <danilgap1464@gmail.com>
2018-12-03 18:08:16 +0400

    model category
    new model category

--- shopsite/app/Models/Item.php
+++ shopsite/app/Models/Item.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'content'
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
