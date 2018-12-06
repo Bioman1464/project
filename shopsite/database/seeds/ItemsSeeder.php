95c3ebb89fe20eb029abaafaf667fed8b9cb23e4
Bioman1464 <danilgap1464@gmail.com>
2018-12-06 20:16:54 +0300

    ItemSeeder

--- shopsite/database/seeds/ItemsSeeder.php
+++ shopsite/database/seeds/ItemsSeeder.php
@@ -13,7 +13,7 @@ class ItemsSeeder extends Seeder
     {
         factory(App\Models\Item::class, 30)->create()->each(function ($item) {
             $category_ids = [];
-            for ($i = 1; $i <= random_int(1, 3); $i++) {
+            for ($i = 1; $i <= random_int(1, 1); $i++) {
                 $category_id = App\Models\Category::all()->random()->id;
                 if (!in_array($category_id, $category_ids)) {
                     $category_ids[] = App\Models\Category::all()->random()->id;
