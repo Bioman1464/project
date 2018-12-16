43334e38e9fe18e7467f57bbf1f616fda66cad54
Nikita <Karelison97@gmail.com>
2018-12-17 02:46:48 +0300

    1

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ["uses"=>"ProductsController@index", 'as'=>'allProducts']);
 
 Route::get('products', ["uses"=>"ProductsController@index", 'as'=>'allProducts']);
 
