29709fd8477ff774da11619098b5043fb4ade660
Bioman1464 <danilgap1464@gmail.com>
2018-12-16 19:23:56 +0300

    kekich

--- shopsite/database/factories/CategoryFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-//$factory->define(App\Models\Category::class, function (Faker $faker) {
-//    return [
-//        'title' => ucfirst(implode(" ", $faker->words(random_int(2, 4))))
-//    ];
-//});
--- shopsite/resources/views/home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('app')
-
-@section('content')
-    <articles></articles>
-@endsection
