db31c866b63ab223d97e6f7cb01664c194d73190
Bioman1464 <danilgap1464@gmail.com>
2018-12-16 17:33:46 +0300

    лул1

--- shop/resources/views/admin/displayProducts.blade.php
+++ shop/resources/views/admin/displayProducts.blade.php
@@ -24,12 +24,13 @@
 @foreach($products as $product)
 <tr>
     <td>{{ $product['id'] }}</td>
-    <td> <img src="<?php echo Storage::url($product['image']); ?>"
-         alt="<?php echo Storage::url($product['image']); ?>" width='100' height='100' style="max-height:220px"></td>
+    <!--<td><img src="{{asset ('storage')}}/{{$product['image']}}" alt="{{asset ('storage')}}/{{$product['image']}}" width='100' height='100' style="max-height:220px"></td>-->
+    <td> <img src="{{ Storage::url('product_images/'.$product['image'])}}"
+         alt="<?php echo Storage::url($product['image']); ?>" width="100" height="100" style="max-height:220px"></td>
     <td>{{ $product['name'] }}</td>
     <td>{{ $product['description'] }}</td>
     <td>{{ $product['type'] }}</td>
-    <td>${{ $product['price'] }}</td>
+    <td>{{ $product['price'] }}</td>
     <td><a href="{{ route('adminEditProductImageForm', ['id'=>$product['id']]) }}" class='btn btn-primary' title="">EditImage</a></td>
     <td><a href="{{ route('adminEditProductForm', ['id'=>$product['id']]) }}" class='btn btn-primary' title="">Edit</a></td>
     <td><a href="" class='btn btn-warning' title="">Delete</a></td>
